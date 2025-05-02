66ed013fefea5143caec1c0f5851d1833330cd2c
CaiooOGarcia <caio.garcia@picpay.com>
2025-05-02 17:35:14 -0300

    compelto cadastrar aluno

--- projeto-germinalab/src/app/components/home/home.component.ts
+++ projeto-germinalab/src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
+  
 })
 export class HomeComponent {
 
